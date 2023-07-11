66034556ccfff0a54ccf6084555c8b336b7aacd3
yubaraj2056 <yubaraj2056>
2023-07-11 16:42:42 +0545

    phone number changed

--- MyFirstTest.cy.js
+++ MyFirstTest.cy.js
@@ -3,7 +3,7 @@ describe('My First Test', function()
 {
     it('Test1', () => {
         cy.visit('https://demo.qa.hisaav.com/login')
-        cy.get(':nth-child(1) > .input-field').type('9988774457')
+        cy.get(':nth-child(1) > .input-field').type('9988774455')
         cy.get('.password-area > .input-field').type('axios@123')
         cy.get('.main-button').click()
        cy.title().should('include', 'Hisaav')
