b0803a6cf0eaf499a34b3a21a639d727dff05653
yubaraj2056 <yubaraj2056>
2023-07-11 16:25:34 +0545

    phn number changed

--- MyFirstTest.cy.js
+++ MyFirstTest.cy.js
@@ -3,8 +3,8 @@ describe('My First Test', function()
 {
     it('Test1', () => {
         cy.visit('https://demo.qa.hisaav.com/login')
-        cy.get(':nth-child(1) > .input-field').type('9865103903')
-        cy.get('.password-area > .input-field').type('12345')
+        cy.get(':nth-child(1) > .input-field').type('9988774455')
+        cy.get('.password-area > .input-field').type('axios@123')
         cy.get('.main-button').click()
        cy.title().should('include', 'Hisaav')
         cy.get('.logo-section > .extra-small-title').should('be.visible')
